2fafe8983c502ce5f5ae314cd1498d8a80858ad8
mariovor <github@hquer.eu>
2018-03-18 12:26:10 +0100

    Implementing the Parcelable interface to the models

--- app/src/main/java/eu/hquer/wekdroid/model/Card.java
+++ app/src/main/java/eu/hquer/wekdroid/model/Card.java
@@ -1,11 +1,17 @@
 package eu.hquer.wekdroid.model;
 
+import android.os.Parcel;
+
 /**
  * Created by mariovor on 03.03.18.
  */
 
 public class Card extends Board{
     String description;
+
+    protected Card(Parcel in) {
+        super(in);
+    }
 
     public String getDescription() {
         return description;
