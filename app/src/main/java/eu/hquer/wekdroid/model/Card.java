62bf2d4f170a6b9bcb05c851876d1a7ec044e456
mariovor <github@hquer.eu>
2018-05-17 20:22:28 +0200

    Make Card model independent from Board to make Parcelable work

--- app/src/main/java/eu/hquer/wekdroid/model/Card.java
+++ app/src/main/java/eu/hquer/wekdroid/model/Card.java
@@ -1,16 +1,61 @@
 package eu.hquer.wekdroid.model;
 
 import android.os.Parcel;
+import android.os.Parcelable;
 
 /**
  * Created by mariovor on 03.03.18.
  */
 
-public class Card extends Board{
+public class Card implements Parcelable {
+    String _id;
+    String title;
     String description;
 
     protected Card(Parcel in) {
-        super(in);
+        _id = in.readString();
+        title = in.readString();
+        description = in.readString();
+    }
+
+    public static final Creator<Card> CREATOR = new Creator<Card>() {
+        @Override
+        public Card createFromParcel(Parcel in) {
+            return new Card(in);
+        }
+
+        @Override
+        public Card[] newArray(int size) {
+            return new Card[size];
+        }
+    };
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel parcel, int i) {
+        parcel.writeString(_id);
+        parcel.writeString(title);
+        parcel.writeString(description);
+    }
+
+    public String get_id() {
+        return _id;
+    }
+
+    public void set_id(String _id) {
+        this._id = _id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
     }
 
     public String getDescription() {
@@ -20,4 +65,5 @@ public class Card extends Board{
     public void setDescription(String description) {
         this.description = description;
     }
+
 }
